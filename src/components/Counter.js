417689cd15ff36a2901d67c95ad39303eba2437a
DaisyChepyegon <daisychepyegon@gmail.com>
2022-07-13 18:11:06 +0300

    Done.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,9 @@ function Counter() {
 
   function increment() {
     setCount(count + 1);
+    //updates the button by 2 when the botton is clicked
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
